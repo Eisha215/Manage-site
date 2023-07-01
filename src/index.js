4c69f6878ebe30c3e1cb3fca628bef6a2e3ace5f
Eisha215 <eishasajid01@gmail.com>
2023-07-01 23:01:30 +0500

    chore: initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var menu = document.getElementById('dropdrown-menu');
+function ToggleMenu() {
+    if(menu.style.display !== 'none'){
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
